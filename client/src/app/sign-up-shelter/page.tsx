e71af0396318156518e719684ac926af097c9057
luccas-specht <luccasspecht70@gmail.com>
2024-05-27 17:19:29 -0300

    feat: add sign up shelter

--- client/src/app/sign-up-shelter/page.tsx
+++ client/src/app/sign-up-shelter/page.tsx
@@ -1,18 +1,25 @@
+'use client';
+
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../../../public/logo-refugio-legal.svg';
 import emailIcon from '../../../public/email.svg';
 import senhaIcon from '../../../public/senha.svg';
+import usersIcon from '../../../public/users.svg';
+import umbrellaIcon from '../../../public/umbrella.svg';
+import smartphoneIcon from '../../../public/smartphone.svg';
+import homeIcon from '../../../public/home.svg';
 
 import { HeaderNoLogo } from '@/components/header/headerNoLogo';
 import styles from './styles.module.css';
 
-export default function signUpShelter() {
+export default function LoginVolunteer() {
   return (
     <>
       <HeaderNoLogo />
       <div className={styles['container']}>
         <Image src={logo} alt="Logo Refugio Legal" />
-        <span>Login</span>
+        <span>Criar conta</span>
         <main>
           <form>
             <div>
@@ -23,10 +30,39 @@ export default function signUpShelter() {
               <Image src={senhaIcon} alt="icone de senha" />
               <input type="password" placeholder="Senha" />
             </div>
+            <div>
+              <Image src={smartphoneIcon} alt="icone de telefone" />
+              <input type="text" placeholder="Número telefone" />
+            </div>
+            <div>
+              <Image src={umbrellaIcon} alt="icone de guarda-chuva" />
+              <input type="text" placeholder="Nome abrigo" />
+            </div>
+            <div>
+              <Image src={usersIcon} alt="icone de usuários" />
+              <input type="number" placeholder="Número de abrigados" />
+            </div>
+            <span className={styles['container-text']}>
+              <Image src={homeIcon} alt="icone de casa" />
+              Endereço do abrigo
+            </span>
+            <div className={styles['wrapper-inputs']}>
+              <input type="text" placeholder="Rua" />
+              <input type="text" placeholder="Número" />
+              <input type="text" placeholder="Bairro" />
+              <input type="text" placeholder="Cidade" />
+              <input type="text" placeholder="Mais Informações (opcional)" />
+            </div>
           </form>
+          <section className={styles['tip']}>
+            <h6>
+              Para confirmar a criação de sua conta, um responsável entrará em
+              contato para aprovar o cadastro do abrigo.
+            </h6>
+          </section>
           <section>
             <button>Fazer Login</button>
-            <button>Criar Conta</button>
+            <Link href={'/sign-up-volunteer'}>Criar conta</Link>
           </section>
         </main>
       </div>
